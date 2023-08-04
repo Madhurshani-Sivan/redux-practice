5f84c770bf0c351199e4d28b81188a2a28130e24
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-08-04 11:30:38 +0530

    add fetch related code to components

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,12 +7,14 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-
       state.totalQuantity++;
-
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -29,9 +31,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
-
       state.totalQuantity--;
-
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
